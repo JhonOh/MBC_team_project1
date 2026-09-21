650e1cb6a5b705ec7d4d987f545c95fc8ebae235
zsoo108 <hhongjisoo@gmail.com>
2026-09-21 17:48:40 +0900

    26.09.21 홍지수

--- odysay/__init__.py
+++ odysay/__init__.py
@@ -5,6 +5,8 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    from .views import place_views
+    app.register_blueprint(place_views.bp)
 
     @app.route('/')
     def index():
@@ -22,8 +24,5 @@ def create_app():
     def map():
         return render_template('map.html')
 
-    @app.route('/hjs')
-    def hjs():
-        return render_template('hjs.html')
 
     return app
