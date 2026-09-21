d1d2aaafa6e407e391d2f66541bef66d60c79a24
박기흠 <sonic7877@gmail.com>
2026-09-21 16:12:41 +0900

    first test

--- odysay/__init__.py
+++ odysay/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
@@ -12,8 +14,12 @@ def create_app():
     def ojh():
         return render_template('ojh.html')
 
-    @app.route('./sjw')
+    @app.route('/sjw')
     def sjw():
         return render_template('sjw.html')
+
+    @app.route('/map.html')
+    def map():
+        return render_template('map.html')
 
     return app
