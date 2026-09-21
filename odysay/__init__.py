1d9fb4b49d4a606cccf2dd3fde8a607bdc1975df
JhonOh <jykojs21@gmail.com>
2026-09-21 17:57:17 +0900

    260921 views 설정

--- odysay/__init__.py
+++ odysay/__init__.py
@@ -1,29 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-db = SQLAlchemy()
+import config
 
+naming_convention = {
+    'ix': 'ix_%(column_0_label)s',
+    'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    'ck': 'ck_%(table_name)s_%(column_0_name)s',
+    'fk': 'fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s',
+    'pk': 'pk_%(table_name)s',
+}
+
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
+migrate = Migrate()
+
+# 애플리케이션 팩토리
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(config)
 
-    @app.route('/')
-    def index():
-        return "flask team project!!"
+    # ORM 초기 설정
+    db.init_app(app)
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith('sqlite'):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
+    from . import models
 
-    @app.route('/ojh')
-    def ojh():
-        return render_template('ojh.html')
+    # 블루프린트 등록
+    from .views import main_views, mapmain_views, sub_views
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(mapmain_views.bp)
+    app.register_blueprint(sub_views.bp)
+    # app.register_blueprint(auth_views.bp)
 
-    @app.route('/sjw')
-    def sjw():
-        return render_template('sjw.html')
-
-    @app.route('/map.html')
-    def map():
-        return render_template('map.html')
-
-    @app.route('/hjs')
-    def hjs():
-        return render_template('hjs.html')
+    # # 필터 등록
+    # from .filter import format_datetime
+    # app.jinja_env.filters['datetime'] = format_datetime
 
     return app
